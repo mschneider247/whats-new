aea853f71640ee94f92d3159e3aeded71ae1ce86
Michael Schneider <michaelschneider@Michaels-MacBook-Pro.local>
2019-10-01 15:02:01 -0600

    Render header with site title, search input and button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import SearchForm from '../SearchForm/SearchForm'
 
 class App extends Component {
   constructor() {
@@ -12,9 +13,9 @@ class App extends Component {
 
   render () {
     return (
-      <div className="app">
-        YOUR CODE GOES HERE!
-      </div>
+      <main className="app">
+        <SearchForm />
+      </main>
     );
   }
 }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {search: ''}
+  }
+
+  handleSearch = event => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
+
+  render() {
+    return (
+      <header>
+        <h1>What's New?</h1>
+        <input
+          type="text"
+          placeholder="Search"
+          name="search"
+          value={this.state.search}
+          onChange={event => this.handleSearch(event)} 
+        />
+        <button>Search News</button>
+      </header>
+    )
+  }
+}
 
 export default SearchForm;
