909ea1176d389df8b7bd2ddc1cbf10ace8c34a3f
Michael Schneider <michaelschneider@Michaels-MBP.PK5001Z>
2019-10-02 21:17:40 -0600

    Add buttons to filter content by title

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = (props) => {
+
+  return(
+    <div>
+      <button onClick={() => {props.filterNews('local')}}>Local News</button>
+      <button onClick={() => {props.filterNews('technology')}}>Technology</button>
+      <button onClick={() => {props.filterNews('entertainment')}}>Entertainment</button>
+      <button onClick={() => {props.filterNews('science')}}>Science</button>
+      <button onClick={() => {props.filterNews('health')}}>Health</button>
+    </div>
+  )
+}
 
 export default Menu;
