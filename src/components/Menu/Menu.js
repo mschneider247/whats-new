dc7cbe24e06481b5440ba0f03f1a9ac1b9198aa2
Michael Schneider <michaelschneider@Michaels-MBP.PK5001Z>
2019-10-02 21:40:14 -0600

    Add menu functionality, clicking menu buttons filters news content

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,15 +3,14 @@ import './Menu.css'
 
 const Menu = (props) => {
 
-  return(
-    <div>
-      <button onClick={() => {props.filterNews('local')}}>Local News</button>
-      <button onClick={() => {props.filterNews('technology')}}>Technology</button>
-      <button onClick={() => {props.filterNews('entertainment')}}>Entertainment</button>
-      <button onClick={() => {props.filterNews('science')}}>Science</button>
-      <button onClick={() => {props.filterNews('health')}}>Health</button>
-    </div>
-  )
-}
+return(
+  <div>
+    <button onClick={() => {props.filterNews('local')}}>Local News</button>
+    <button onClick={() => {props.filterNews('technology')}}>Technology</button>
+    <button onClick={() => {props.filterNews('entertainment')}}>Entertainment</button>
+    <button onClick={() => {props.filterNews('science')}}>Science</button>
+    <button onClick={() => {props.filterNews('health')}}>Health</button>
+  </div>
+)}
 
 export default Menu;
