77be37733f21bc7fcc77ffd8f9182d9e0e3385f9
Michael Schneider <michaelschneider@Michaels-MBP.PK5001Z>
2019-10-05 22:02:57 -0600

    Fetch news data from url source, show loading screen

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const newsData = {
+  default: [{
+      id: 0,
+      headline: 'Loading...',
+      description: 'Please Wait to',
+      img: 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/source.gif'}],
+}
+
+const parseData = (data) => {
+  let dataKeys = Object.keys(data)
+  dataKeys.forEach(key => {
+    newsData[key] = data[key];
+  });
+};
+
+fetch("https://whats-new-api.herokuapp.com/api/v1/news")
+  .then(data => data.json())
+  .then(data => parseData(data))
+  .then(ReactDOM.render( <App data={newsData} /> , document.getElementById('root')))
+  .catch(err => console.log(err));
+
